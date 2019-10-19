3bd0a9509f4cada930e0d8453303725b47eb4f3d
Will Ness <willness210@gmail.com>
2019-10-19 17:36:30 -0600

    c++ and python sample bots

---
+++ bots/SampleCPPBot/SampleCPPBot.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    while(true){
+        string mystr;
+        getline(cin, mystr);
+        cout << mystr << "\n";
+    }
+}
---
+++ bots/SamplePythonBot/SamplePythonBot.py
@@ -0,0 +1,7 @@
+def main():
+    while True:
+        program_input = raw_input()
+        print(program_input)
+
+if __name__ == "__main__":
+    main()
