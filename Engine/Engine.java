5e1c96e1f4074f61500aad378e49ead72c8785ac
Will Ness <willness210@gmail.com>
2019-10-19 10:29:27 -0600

    added config support

--- Engine/Engine.java
+++ Engine/Engine.java
@@ -1,17 +1,32 @@
+import java.util.Properties;
 import java.util.Random;
 
 public class Engine{
     // ENGINE VARIABLES
     private long mapSeed;
     private String[] botFilepaths;
+    private Properties config;
 
     // CONSTRUCTORS
-    public Engine(String[] botFilepaths){
+    public Engine(Properties config, String[] botFilepaths){
+        this.config = config;
         this.botFilepaths = botFilepaths;
         this.mapSeed = (new Random()).nextLong();
     }
-    public Engine(String[] botFilepaths, int mapSeed){
+    public Engine(Properties config, String[] botFilepaths, int mapSeed){
+        this.config = config;
         this.botFilepaths = botFilepaths;
         this.mapSeed = mapSeed;
+    }
+
+    // STARTUP STUFF
+
+
+    // CONFIG FUNCS
+    public String getGameName(){
+        return config.getProperty("game_name");
+    }
+    public int getNumPlayers(){
+        return Integer.parseInt(config.getProperty("num_players"));
     }
 }
