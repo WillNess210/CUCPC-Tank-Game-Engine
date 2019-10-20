e89a768826fac9e5fca20af710bfb8f5f049e42f
Will Ness <willness210@gmail.com>
2019-10-20 09:58:28 -0600

    added program run back into run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 clear
 javac $(find -name "*.java" -not -path "./bots/*")
-#java Main bots/SampleJavaBot bots/SampleJavaBot
+java Main bots/SampleJavaBot bots/SampleJavaBot
 #java Main bots/SampleJavaBot bots/SamplePythonBot
 #java Main bots/SampleJavaBot bots/SampleCPPBot
 find -name "*.class" -not -path "./bots/*" -delete
